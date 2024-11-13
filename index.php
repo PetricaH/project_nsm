5c1fe040fadbf0e05005a2479de72250e1ffc765
PetricaH <petricahphp@gmail.com>
2024-11-13 21:07:17 +0200

    update

--- index.php
+++ index.php
@@ -17,5 +17,18 @@
         <img src="static/images/me.png" id="me_hero_section_img" alt="Me">
         <div class="hero_bg_top_layer"></div>
     </section>
+    <section id="programming_section">
+        <div class="programming_logos_part">
+            <div class="programming_techs_container">
+                <img src="static/images/square-pattern-white-v2.png" class="white_pattern" alt="">
+                <img src="static/images/programming_techs_logos/git-icon.svg" class="programming_tech_img git_img" alt="">
+                <img src="static/images/programming_techs_logos/github.svg" class="programming_tech_img github_img" alt="">
+                <img src="static/images/programming_techs_logos/php-alt.svg" class="programming_tech_img php_img" alt="">
+                <img src="static/images/programming_techs_logos/javascript.svg" class="programming_tech_img js_img" alt="">
+                <img src="static/images/programming_techs_logos/css-3.svg" class="programming_tech_img css_img" alt="">
+                <img src="static/images/programming_techs_logos/html-5.svg" class="programming_tech_img html_img" alt="">
+            </div>
+        </div>
+    </section>
 </body>
 </html>
